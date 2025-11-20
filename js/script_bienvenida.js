73f9010edbc8b221089775b9f8a787b2024709d1
ZDxrKyZ <fabriziombenitez@gmail.com>
2025-11-20 20:34:53 -0300

    Delete js directory

--- js/script_bienvenida.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let nombre = prompt("Ingrese su Nombre:");
-    let apellido = prompt("Ingrese su Apellido:");
-
-    if (nombre && apellido) {
-        document.getElementById("titulo-bienvenida").innerText =
-            `Bienvenido/a, ${nombre} ${apellido}`;
-    }
-});
